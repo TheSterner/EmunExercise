1d097b139a6717f6a95b7111968d21d78f09279e
William B McAllister <blakemcallister77@gmail.com>
2018-10-21 22:15:58 -0400

    pg134

--- EmunExercise/Program.cs
+++ EmunExercise/Program.cs
@@ -10,6 +10,40 @@ namespace EmunExercise
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Please enter the current day of the week.");
+			string userDay = Console.ReadLine();
+			DaysOfTheWeek day;
+
+			try
+			{
+				day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userDay);
+				Console.WriteLine(day);
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Please enter a valid weekday.");
+				Console.WriteLine(ex.Message);
+				Console.ReadLine();
+				
+			}
+			
+
+			Console.ReadLine();
+
+
+
+
+		}
+		enum DaysOfTheWeek
+		{
+			Monday,
+			Tuesday,
+			Wednesday,
+			Thursday,
+			Friday,
+			Saturday,
+			Sunday
 		}
 	}
 }
